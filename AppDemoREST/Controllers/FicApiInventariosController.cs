1282293b497c02dfa05b1444aa938b184767762b
MIGUEL ANGEL DURAN OCAMPO <mianduranoc@ittepic.edu.mx>
2019-05-02 02:40:20 -0600

    FIC

--- AppDemoREST/Controllers/FicApiInventariosController.cs
+++ AppDemoREST/Controllers/FicApiInventariosController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+//using System.Data.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using AppDemoREST.Data;
+using static AppDemoREST.Models.FicModInventarios;
 
 namespace AppDemoREST.Controllers
 {
@@ -18,10 +20,12 @@ namespace AppDemoREST.Controllers
         {
             FicLoDBContext = FicPaBDContext;
         }
+        //consulta completa, consulta por id cedi, insert inventario, modificacion inventario, eliminar inventario
         [HttpGet]
         [Route("/api/inventarios/invacucon")]
         public async Task<IActionResult>FicApiGetListInventarios([FromQuery]int cedi)
         {
+
             var zt_inventarios = (from data_inv in FicLoDBContext.zt_inventarios where data_inv.IdCEDI == cedi select data_inv).ToList();
             if (zt_inventarios.Count()>0)
             {
@@ -33,6 +37,81 @@ namespace AppDemoREST.Controllers
                 zt_inventarios = zt_inventarios.ToList();
                 return Ok(zt_inventarios);
             }
+        }
+        [HttpGet]
+        [Route("/api/inventarios")]
+        public async Task<IActionResult> FicApiGetListInventarios()
+        {
+            var zt_inventarios = (from data_inv in FicLoDBContext.zt_inventarios select data_inv).ToList();
+            if (zt_inventarios.Count() > 0)
+            {
+                zt_inventarios = zt_inventarios.ToList();
+                return Ok(zt_inventarios);
+            }
+            else
+            {
+                zt_inventarios = zt_inventarios.ToList();
+                return Ok(zt_inventarios);
+            }
+        }
+        [HttpPost]
+        [Route("/api/inventarios")]
+        public async Task<IActionResult> FicApiSetInventario([FromForm] int id, [FromForm]short cedi, [FromForm]string sap, [FromForm] DateTime fecha, [FromForm] string user)
+        {
+            Console.WriteLine(id);
+            zt_inventarios inventario = new zt_inventarios();
+            inventario.IdInventario = id;
+            inventario.IdCEDI = cedi;
+            inventario.IdInventarioSAP = sap;
+            inventario.FechaReg = fecha;
+            inventario.UsuarioReg = user;
+            inventario.Activo = "S";
+            inventario.Borrado = "N";
+            FicLoDBContext.zt_inventarios.Add(inventario);
+            FicLoDBContext.SaveChanges();
+            return Ok(inventario);
+            
+        }
+        [HttpDelete]
+        [Route("/api/inventarios")]
+        public async Task<IActionResult> FicApiDeleteInventario([FromQuery] int id)
+        {
+            zt_inventarios inventario = new zt_inventarios();
+            inventario.IdInventario = id;
+            try
+            {
+                FicLoDBContext.zt_inventarios.Remove(inventario);
+                FicLoDBContext.SaveChanges();
+                return Ok(inventario);
+            }
+            catch(Microsoft.EntityFrameworkCore.DbUpdateException e)
+            {
+                Dictionary<String, String> err = new Dictionary<string, string>();
+                err.Add("err","No se encontraron registros");
+                return Ok(err);
+            }  
+        }
+        [HttpPut]
+        [Route("/api/inventarios")]
+        public async Task<IActionResult> FicApiUpdateInventario([FromForm] int id, [FromForm]string sap, [FromForm] DateTime fecha, [FromForm] string user)
+        {
+            try
+            {
+                var inventario = FicLoDBContext.zt_inventarios.First(a => a.IdInventario == id);
+                inventario.IdInventario = id;
+                inventario.IdInventarioSAP = sap;
+                inventario.FechaUltMod = fecha;
+                inventario.UsuarioMod = user;
+                FicLoDBContext.SaveChanges();
+                return Ok(inventario);
+            }
+            catch(Exception e)
+            {
+                Dictionary<String, String> err = new Dictionary<string, string>();
+                err.Add("err", "No se encontraron registros");
+                return Ok(err);
+            }
+            
         }
         // GET: api/FicApiInventarios
         /*[HttpGet]
